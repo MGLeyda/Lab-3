e6f6f59056f8ea9580b0a29b9ce87d4a9cdb4ff5
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2021-11-06 16:31:53 -0500

    hello page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import * as React from 'react'
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
+      <title>Home Page!</title>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>

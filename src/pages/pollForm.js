2ffc059cd7bf61c0b8783f91024b01cef1c6d1a5
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2021-11-12 11:55:01 -0600

    poll form

---
+++ src/pages/pollForm.js
@@ -0,0 +1,96 @@
+import React from "react"
+export default class PollFormPage extends React.Component {
+  state = {
+    Title: "",
+    Location: "",
+    Notes: "",
+    RestrictVperPoll: "",
+    RestrictVperPar: "",
+    Invite: "",
+  }
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+  handlePublish = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.title} ${this.state.location}`)
+  }
+  render() {
+    return (
+      <form onSubmit={this.handlePublish}>
+        <h1>New Poll</h1>
+        <br />
+        <label>
+          Title:    
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Location:  
+          <input
+            type="text"
+            name="location"
+            value={this.state.location}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+         <label>Time Zone </label> 
+         <select>
+            <option value = "def">No Time Zone</option>
+            <option value = "CT">Central Standard Time</option>
+            <option value = "MT">Moutain Standard Time</option>
+            <option value = "PT">Pacific Standard Time</option>
+            <option value = "AT">Alaska Standard Time</option>
+            <option value = "HT">Hawaii-Aleutian Standard Time</option>
+        </select>
+        <br />
+        <label>Notes/Comments: </label>
+        <br />
+        <textarea name="Text1" cols="40" rows="5"></textarea>
+        <br />
+        <label>Restrict Number of Votes per Poll: </label> 
+         <select>
+            <option value = "na">No Restriction</option>
+            <option value = "1">1</option>
+            <option value = "2">2</option>
+            <option value = "3">3</option>
+            <option value = "4">4</option>
+            <option value = "5">5</option>
+        </select>
+        <br />
+        <label>Restrict Number of Votes per Participant: </label> 
+         <select>
+            <option value = "na">No Restriction</option>
+            <option value = "1">1</option>
+            <option value = "2">2</option>
+            <option value = "3">3</option>
+            <option value = "4">4</option>
+            <option value = "5">5</option>
+        </select>
+        <br />
+        <label>
+          Invite Participant(s):  
+          <input
+            type="text"
+            name="invite"
+            value={this.state.location}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="publish">Publish</button>
+      </form>
+    )
+  }
+}

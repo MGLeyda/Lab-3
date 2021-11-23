8785ee99b2fd5ec41a1eff2cac685ee7ea807738
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2021-11-22 23:11:44 -0600

    navigate to new poll

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,10 +3,15 @@ import { navigate } from 'gatsby';
 
 function Dashboard(props){
     
+    const handleNewPoll = () =>{
+        navigate('/pollForm/')
+    }
     return (
         <center>
         <div>
             <h1>Administrative Dashboard</h1>
+            <br /><br />
+            <button type="newPoll" onClick={handleNewPoll} >Create New Poll</button> 
         </div>
         </center>
       );

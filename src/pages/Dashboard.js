a45dba926f0482a1467bfdb6a271a9babaa7d28d
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2021-11-22 23:06:29 -0600

    updated routing and login page format

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+
+function Dashboard(props){
+    
+    return (
+        <center>
+        <div>
+            <h1>Administrative Dashboard</h1>
+        </div>
+        </center>
+      );
+}
+export default Dashboard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,47 @@
-import React from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import AdminPage from "./adminHome.js";
-export default class IndexPage extends React.Component {
-  state = {
-    Username: "",
-    Password: "",
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
+
+function Login(props) {
+  const username = useFormInput('');
+  const password = useFormInput('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+ 
+  // handle button click of login form
+  const handleLogin = () => {
+    navigate('/Dashboard/')
   }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
+ 
+  return (
+    <center>
+    <div>
+    <h1 style={{color: "green"}}>Login</h1>
+      <div>
+        Username<br />
+        <input type="text" {...username} autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" {...password} autoComplete="new-password" />
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+    </div>
+    </center>
+  )
+  
+}
+ 
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+ 
+  const handleChange = e => {
+    setValue(e.target.value);
   }
-  handleSubmit = event => {
-    <Router>
-      <main>
-        <nav>
-          <ul>
-          <li><a href = "/adminHome.js">AdminPage</a></li>
-          </ul>
-        </nav>
-        <Route path = "/adminHome.js" component={AdminPage} />
-      </main>
-    </Router>
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Login</h1>
-        <br />
-        <label>
-          Username:    
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Password:  
-          <input
-            type="text"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    )
+  return {
+    value,
+    onChange: handleChange
   }
 }
+ 
+export default Login;

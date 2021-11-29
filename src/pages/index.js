803d535a45a06c84f638eea80f97bb38226de342
MGLeyda <79680573+MGLeyda@users.noreply.github.com>
2021-11-29 11:16:45 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,49 @@
-import React from "react"
-import { heading } from '../components/login.module.css'
-export default class IndexPage extends React.Component {
-  state = {
-    Username: "",
-    Password: "",
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
+
+function Login(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+ 
+  // handle button click of login form
+  const handleLogin = event => {
+      if(username === 'username' && password === 'password'){
+        navigate('/Dashboard/')
+      }
   }
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
-  }
-  handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.Username} ${this.state.password}!`)
-  }
-  render() {
-    return (
+ 
+  return (
     <center>
-      <form onSubmit={this.handleSubmit}>
-        <h1 style={{color: "red"}}>Login</h1>
-        <br />
-        <label>
-          Username:    
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Password:  
-          <input
-            type="text"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-     
-        <button type="submit">Submit</button>
-      </form>
+    <div>
+    <h1 style={{color: "green"}}>Login</h1>
+      <div>
+        Username<br />
+        <input type="text" onChange = {event => setUsername(event.target.value)}/>
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" onChange = {event => setPassword(event.target.value)}/>
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+    </div>
     </center>
-    )
+  )
+  
+}
+ 
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+ 
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
   }
 }
+ 
+export default Login;

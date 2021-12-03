aa5f5e90bba7271cfec74a54c8b58cd3ee01a662
Glitch (metal-shadow-winter) <none>
2021-12-03 04:35:56 +0000

    🛳🐪 Updated with Glitch

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -16,26 +16,147 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const dbRef = ref(getDatabase());
-get(child(dbRef, "/")).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
+// THESE VARIABLES CONTAIN THE DATA FROM FIREBASE
+var pollTitle = "default";
+var pollLocation = "default";
+var pollTimeZone = "NTZ";
+var pollNotes = "";
+var pollDateDD = "01";
+var pollDateMM = "01";
+var pollDateYY = "2022";
+var pollStartTimeHH = "01";
+var pollStartTimeMM = "01";
+var pollStartAMPM = "AM";
+var pollEndTimeHH = "01";
+var pollEndTimeMM = "01";
+var pollEndAMPM = "AM";
+var pollTimeSlots = "1";
+var pollRestrictVotesPerson = "na";
+var pollRestrictVotesPoll = "na";
+var pollRemind = "na";
+var pollInvite = "na";
 
+// Getting data from Firebase
+const dbRef = ref(getDatabase());
+  get(child(dbRef, "/")).then((snapshot) => {
+    get(child(dbRef, "/title")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollTitle = snapshot.val();
+        console.log(pollTitle);
+      } 
+    })
+    get(child(dbRef, "/location")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollLocation = snapshot.val();
+        console.log(pollLocation);
+      } 
+    })
+    get(child(dbRef, "/timeZone")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollTimeZone = snapshot.val();
+        console.log(pollTimeZone);
+      } 
+    })
+    get(child(dbRef, "/notes")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollNotes = snapshot.val();
+        console.log(pollNotes);
+      } 
+    })
+    get(child(dbRef, "/dateDD")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollDateDD = snapshot.val();
+        console.log(pollDateDD );
+      } 
+    })
+    get(child(dbRef, "/dateMM")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollDateMM = snapshot.val();
+        console.log(pollDateMM );
+      } 
+    })
+    get(child(dbRef, "/dateYY")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollDateYY= snapshot.val();
+        console.log(pollDateYY );
+      } 
+    })
+    get(child(dbRef, "/startTimeHH")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollStartTimeHH= snapshot.val();
+        console.log(pollStartTimeHH );
+      } 
+    })
+    get(child(dbRef, "/startTimeMM")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollStartTimeMM= snapshot.val();
+        console.log(pollStartTimeHH);
+      } 
+    })
+    get(child(dbRef, "/startAMPM")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollStartAMPM= snapshot.val();
+        console.log(pollStartAMPM );
+      } 
+    })
+        get(child(dbRef, "/endTimeHH")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollEndTimeHH= snapshot.val();
+        console.log(pollEndTimeHH );
+      } 
+    })
+    get(child(dbRef, "/endTimeMM")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollEndTimeMM= snapshot.val();
+        console.log(pollEndTimeHH);
+      } 
+    })
+    get(child(dbRef, "/endAMPM")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollEndAMPM= snapshot.val();
+        console.log(pollEndAMPM );
+      } 
+    })
+    get(child(dbRef, "/timeSlots")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollTimeSlots= snapshot.val();
+        console.log(pollTimeSlots );
+      } 
+    })
+    get(child(dbRef, "/restrictVotesPoll")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollRestrictVotesPoll= snapshot.val();
+        console.log(pollRestrictVotesPoll);
+      } 
+    })
+    get(child(dbRef, "/restrictVotesPerson")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollRestrictVotesPerson= snapshot.val();
+        console.log(pollRestrictVotesPerson);
+      } 
+    })
+    get(child(dbRef, "/invite")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollInvite= snapshot.val();
+        console.log(pollInvite);
+      } 
+    })
+    get(child(dbRef, "/remind")).then((snapshot) => {
+      if (snapshot.exists()) {
+        pollRemind= snapshot.val();
+        console.log(pollRemind);
+      } 
+    })
+  })
 
 export default function user(){
 
 
 return(
     <div className="login-container">
-    <h2><div> Example Title </div></h2>
+    <h2><div> {pollTitle} </div></h2>
         <hr></hr>   
-      <label>Location: Seaman's Center</label> <br />
+      <label>Location: {pollLocation}</label> <br />
       <label> Your Name: </label> 
       <input></input>
       <br />
@@ -95,51 +216,5 @@ return(
     </div>
       
 );
-  
-  const handlePublish = ()=> {
-    
-    const editPoll = document.getElementById("create-poll");
-        const title = editPoll.title.value = "title";
-        const location = editPoll.location.value;
-        const timeZone = editPoll.timeZone.value;
-        const notes = editPoll.notes.value;
-        const dateMM = editPoll.dateMM.value;
-        const dateDD = editPoll.dateDD.value;
-        const dateYY= editPoll.dateYY.value;
-        const startTimeHH = editPoll.startTimeHH.value;
-        const startTimeMM = editPoll.startTimeMM.value;
-        const startAMPM= editPoll.startAMPM.value;
-        const endTimeHH = editPoll.endTimeHH.value;
-        const endTimeMM = editPoll.endTimeMM.value;
-        const endAMPM= editPoll.endAMPM.value;
-        const timeSlots= editPoll.timeSlots.value;
-        const restrictVotesPoll= editPoll.restrictVotesPoll.value;
-        const restrictVotesPerson= editPoll.restrictVotesPerson.value;
-        const invite= editPoll.invite.value;
-        const remind= editPoll.remind.value;
-  
-    const db = getDatabase();
-        set(ref(db, '/'), {
-          title: title,
-          location: location,
-          timeZone: timeZone,
-          notes: notes,
-          dateMM : dateMM,
-          dateDD : dateDD,
-          dateYY : dateYY,
-          startTimeHH : startTimeHH,
-          startTimeMM : startTimeMM,
-          startAMPM : startAMPM,
-          endTimeHH : endTimeHH,
-          endTimeMM : endTimeMM,
-          endAMPM : endAMPM,
-          timeSlots : timeSlots,
-          restrictVotesPoll : restrictVotesPoll,
-          restrictVotesPerson : restrictVotesPerson,
-          invite : invite,
-          remind : remind,
-        });
-  
-  }
   
 }
